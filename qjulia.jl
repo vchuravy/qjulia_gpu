cd6e6fffb86e0cf225153eb83f4cdeebe9937c41
Simon Danisch <sdanisch@gmail.com>
2014-06-05 23:10:10 +0200

    started creating OpenCL context group

--- qjulia.jl
+++ qjulia.jl
@@ -1,10 +1,51 @@
 import OpenCL
+using ModernGL, GLWindow, GLFW
 const cl = OpenCL
 
-# Setup program parameter
+wglGetCurrentContext() 	= ccall((:wglGetCurrentContext, "opengl32"), Ptr{Void}, ())
+wglGetCurrentDC() 		= ccall((:wglGetCurrentDC, "opengl32"), Ptr{Void}, ())
 
+const window = createWindow([512, 512], "opencl&opengl yeah")
+
+platformid =  	cl.CL_platform_id[0]
+platformNum =  	cl.CL_uint[0]
+cl.api.clGetPlatformIDs(1, platformid, platformNum)
+platform = platformid[1]
+
+const props  = cl.CL_context_properties[
+cl.CL_GL_CONTEXT_KHR, wglGetCurrentContext(),
+cl.CL_WGL_HDC_KHR, wglGetCurrentDC(),
+cl.CL_CONTEXT_PLATFORM , platform, 0]
+
+const devices = cl.devices(:gpu)
+const device = devices[1]
+
+
+# Setup OpenCL
+const ctx = cl.Context(props, devices)
+const queue = cl.CmdQueue(ctx)
+if !device[:has_image_support]
+	error("Device $device has no image support. Aborting.")
+end
+
+# Setup program parameter
 const width = 512 # Also needs changing in qjulia_kernel.cl
 const height = 512 # Also needs changing in qjulia_kernel.cl
+
+gl_texture_id = GLuint[0]
+glGenTextures(1, gl_texture_id)
+gl_texture_id = gl_texture_id[1]
+@assert gl_texture_id > 0
+glBindTexture(GL_TEXTURE_2D, gl_texture_id)
+glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_FLOAT, C_NULL);
+glBindTexture(GL_TEXTURE_2D, 0)
+
+err_code = Array(cl.CL_int, 1)
+const image = cl.api.clCreateFromGLTexture2D(ctx.id, cl.CL_MEM_READ_WRITE, GL_TEXTURE_2D, 0, gl_texture_id, err_code)
+
+if err_code[1] != cl.CL_SUCCESS
+    println(err_code[1])
+end
 
 const ε = 0.003f0
 
@@ -17,14 +58,6 @@ const μT = 0.0f0
 const μA = [-0.278f0, -0.479f0, 0.0f0, 0.0f0]
 const μB = [0.278f0, 0.479f0, 0.0f0, 0.0f0] 
 const μC = [-0.278f0, -0.479f0, -0.231f0, 0.235f0]
-
-# Setup OpenCL
-const device, ctx, queue = cl.create_compute_context()
-
-if !device[:has_image_support]
-	error("Device $device has no image support. Aborting.")
-end
-
 const kernelsource = open(readall, joinpath(dirname(Base.source_path()), "qjulia_kernel.cl"))
 const qjulia_program = cl.Program(ctx, source=kernelsource) |> cl.build!
 const qjulia_kernel = cl.Kernel(qjulia_program, "QJuliaKernel")
